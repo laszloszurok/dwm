4ab6ce43ff93177e7f69d07fe1d59b88d8962248
László Szurok <szuroklaszlo97@gmail.com>
2020-12-11 14:39:48 +0100

    initial commit

---
+++ colors.h
@@ -0,0 +1,28 @@
+static const char norm_fg[] = "#e0dbd2";
+static const char norm_bg[] = "#191b28";
+static const char norm_border[] = "#333";
+
+static const char sel_fg[] = "#e0dbd2";
+static const char sel_bg[] = "#563d7c";
+static const char sel_border[] = "#663d9c";
+
+static const char not_sel_fg[] = "#e0dbd2";
+static const char not_sel_bg[]  = "#3e4050";
+static const char not_sel_border[] = "#3c3933";
+
+static const char hid_fg[] = "#e0dbd2";
+static const char hid_bg[] = "#191b28";
+static const char hid_border[] = "#000";
+
+static const char urg_fg[] = "#e0dbd2";
+static const char urg_bg[] = "#e05D6A";
+static const char urg_border[] = "#e05D6A";
+
+static const char *colors[][3]      = {
+    /*                 fg          bg          border                         */
+    [SchemeNorm]   = { norm_fg,    norm_bg,    norm_border }, 
+    [SchemeSel]    = { sel_fg,     sel_bg,     sel_border },  // the focused win
+    [SchemeUrg]    = { urg_fg,     urg_bg,     urg_border },
+	[SchemeHid]    = { hid_fg,     hid_bg,     hid_border },
+    [SchemeNotSel] = { not_sel_fg, not_sel_bg, not_sel_border }  // unfocused wins
+};
---
+++ patches/shiftview.c
@@ -0,0 +1,36 @@
+/* This function makes it possible to cycle through tags. It jumps over empty tags. 
+ * Copy the function to dwm.c under the function implementations part and copy this under the function declarations part: 
+ * static void shiftview(const Arg *arg); 
+ * */
+
+void
+shiftview(const Arg *arg)
+{
+	Arg a;
+	Client *c;
+	unsigned visible = 0;
+	int i = arg->i;
+	int count = 0;
+	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
+
+	do {
+		if(i > 0) // left circular shift
+			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
+
+		else // right circular shift
+			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
+
+                // Check if tag is visible
+		for (c = selmon->clients; c && !visible; c = c->next)
+			if (nextseltags & c->tags) {
+				visible = 1;
+				break;
+			}
+		i += arg->i;
+	} while (!visible && ++count < 10);
+
+	if (count < 10) {
+		a.i = nextseltags;
+		view(&a);
+	}
+}
